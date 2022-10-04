6d4064f7bd37a9cccbb9fb69acb8c59a39c30895
Shiqan <ferron.saan@live.nl>
2022-10-04 19:01:39 +0200

    use RateLimiter for api calls

--- src/FundaApi.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/FundaApi.Core/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
-﻿using FundaApi.Core.Contracts;
+﻿using ComposableAsync;
+using FundaApi.Core.Contracts;
 using FundaApi.Core.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Extensions.Http;
+using RateLimiter;
+using System;
 using System.Net;
 
 namespace FundaApi.Core.Extensions;
@@ -17,13 +20,19 @@ public static class ServiceCollectionExtensions
           .Validate(o => !string.IsNullOrEmpty(o.ApiKey), "Api key must be provided.");
 
         services.AddHttpClient<IBrokerApi, BrokerApi>()
+                .ConfigurePrimaryHttpMessageHandler(configureHandler =>
+                {
+                    return TimeLimiter
+                            .GetFromMaxCountByInterval(100, TimeSpan.FromSeconds(60))
+                            .AsDelegatingHandler();
+                })
                 .SetHandlerLifetime(TimeSpan.FromMinutes(5))
                 .AddPolicyHandler((services, request) => HttpPolicyExtensions
                        .HandleTransientHttpError()
                        .OrResult(msg => msg.StatusCode == HttpStatusCode.Unauthorized)
                        .WaitAndRetryAsync(
                             retryCount: 3,
-                            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(60),
+                            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
                             onRetry: (retryAttempt, delay) =>
                             {
                                 services.GetService<ILogger<BrokerApi>>()?.LogTrace("Delaying for {delay} seconds, then making retry.", delay.TotalSeconds);
